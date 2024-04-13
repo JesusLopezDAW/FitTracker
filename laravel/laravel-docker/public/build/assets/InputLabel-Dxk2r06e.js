ddac5fea367c4ffdcbedefec3d72bfb874b57df7
Jesus <jesuslpzz123@gmail.com>
2024-04-13 16:10:48 +0200

    holatio

---
+++ laravel/laravel-docker/public/build/assets/InputLabel-Dxk2r06e.js
@@ -0,0 +1,1 @@
+import{o as e,e as t,t as o,r as n}from"./app-CoktfNj0.js";const r={class:"block font-medium text-sm text-gray-700 dark:text-gray-300"},l={key:0},c={key:1},u={__name:"InputLabel",props:{value:String},setup(a){return(s,_)=>(e(),t("label",r,[a.value?(e(),t("span",l,o(a.value),1)):(e(),t("span",c,[n(s.$slots,"default")]))]))}};export{u as _};
