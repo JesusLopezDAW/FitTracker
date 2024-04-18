bdb779cb71593eaf8ef4b79c802e80e64bb1e5ea
Jesus <jesuslpzz123@gmail.com>
2024-04-18 22:37:53 +0200

    migraciones

--- laravel/laravel-docker/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravel/laravel-docker/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
+            $table->string('username')->unique();
+            $table->integer('phone_number')->unique();
+            $table->enum('gender', ['male', 'female', 'other', 'prefer_not_to_say']);
+            $table->integer('birthdate');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
