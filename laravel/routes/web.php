0ca784061115b2ba3b607770a2a6b432d6d63cc3
Alvaro Hernandez Andres <a.hernandez@binary-factory.de>
2024-05-01 20:15:02 +0200

    Rutas refactor

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -12,32 +12,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Rutas AdminLTE
-Route::get('/dashboard', function () {
-    return view('admin.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
+// Cambiar
 Route::get('/users', [UserController::class, 'listUsers'])->middleware(['auth', 'verified'])->name('users');
 
 Route::get('/users/{userName}', function () {
     return view('admin.user-details');
-})->middleware(['auth', 'verified'])->name('user-details');
+})->name('user-details');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth', 'verified'])->group(function () {
+    // Rutas AdminLTE
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
+
+    Route::controller(ProfileController::class)->group(function () {
+        Route::get('/profile', 'edit')->name('profile.edit');
+        Route::patch('/profile', 'update')->name('profile.update');
+        Route::delete('/profile','destroy')->name('profile.destroy');
+    });
+
+    Route::resource("/user", UserController::class);
+    Route::resource("/food", FoodController::class);
+    Route::resource("/exercise", FoodController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 // Listar -> Index (GET)
 // Insertar -> Store (POST)
 // Listar usuario especifico -> Show (GET): /user/{user} 
 // Update (PUT/PATCH): /user/{user} - Esta ruta actualiza un usuario existente en la base de datos. También podría ser /user/{user}/edit.
 // Destroy (DELETE): /user/{user} - Esta ruta elimina un usuario de la base de datos.
-Route::resource("/user",UserController::class);
-Route::resource("/food",FoodController::class);
-Route::resource("/exercise",ExerciseController::class);
+
 
 // Route::get('/user/{userName}', [UserController::class, 'userDetails']);
