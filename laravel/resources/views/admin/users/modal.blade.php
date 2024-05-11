ad3ecd50a0f57dfa1898c02a66d8cb8d03594224
Jesus <jesuslpzz123@gmail.com>
2024-05-11 16:19:20 +0200

    modal

--- laravel/resources/views/admin/users/modal.blade.php
+++ laravel/resources/views/admin/users/modal.blade.php
@@ -15,80 +15,127 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="name"><i class="fas fa-user"></i> Nombre</label>
-                                <input type="text" class="form-control" id="name">
+                                <label for="name">Nombre</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" id="name">
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="surname"><i class="fas fa-user"></i> Apellido</label>
-                                <input type="text" class="form-control" id="surname">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="username"><i class="fas fa-user"></i> Nombre de Usuario</label>
-                                <input type="text" class="form-control" id="username">
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="rol"><i class="fas fa-user-cog"></i> Rol</label>
-                                <select class="form-control" id="rol">
-                                    <option value="user">Usuario</option>
-                                    <option value="admin">Administrador</option>
-                                </select>
+                                <label for="surname">Apellido</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" id="surname">
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="phone_number"><i class="fas fa-phone"></i> Número de Teléfono</label>
-                                <input type="number" class="form-control" id="phone_number">
+                                <label for="username">Nombre de Usuario</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" id="username">
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="birthdate"><i class="fas fa-calendar-alt"></i> Fecha de Nacimiento</label>
-                                <input type="date" class="form-control" id="birthdate">
+                                <label for="rol">Rol</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-user-cog"></i></span>
+                                    </div>
+                                    <select class="form-control" id="rol">
+                                        <option value="user">Usuario</option>
+                                        <option value="admin">Administrador</option>
+                                    </select>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="email"><i class="fas fa-envelope"></i> Correo Electrónico</label>
-                                <input type="email" class="form-control" id="email">
+                                <label for="phone_number">Número de Teléfono</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-phone"></i></span>
+                                    </div>
+                                    <input type="number" class="form-control" id="phone_number">
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="gender"><i class="fas fa-venus-mars"></i> Género</label>
-                                <select class="form-control" id="gender">
-                                    <option value="male">Masculino</option>
-                                    <option value="female">Femenino</option>
-                                    <option value="other">Otro</option>
-                                    <option value="prefer_not_to_say">Prefiero no decirlo</option>
-                                </select>
+                                <label for="birthdate">Fecha de Nacimiento</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
+                                    </div>
+                                    <input type="date" class="form-control" id="birthdate">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="email">Correo Electrónico</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                    </div>
+                                    <input type="email" class="form-control" id="email">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="gender">Género</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-venus-mars"></i></span>
+                                    </div>
+                                    <select class="form-control" id="gender">
+                                        <option value="male">Masculino</option>
+                                        <option value="female">Femenino</option>
+                                        <option value="other">Otro</option>
+                                        <option value="prefer_not_to_say">Prefiero no decirlo</option>
+                                    </select>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group">
-                                <label for="password"><i class="fas fa-lock"></i> Contraseña</label>
-                                <input type="password" class="form-control" id="password">
+                                <label for="password">Contraseña</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                                    </div>
+                                    <input type="password" class="form-control" id="password">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fas fa-times"></i> Cancelar</button>
-                <button type="button" class="btn btn-primary" id="btnGuardarUsuario"><i class="fas fa-save"></i> Guardar Usuario</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fas fa-times"></i>
+                    Cancelar</button>
+                <button type="button" class="btn btn-primary" id="btnGuardarUsuario"><i class="fas fa-save"></i>
+                    Guardar Usuario</button>
             </div>
         </div>
     </div>
