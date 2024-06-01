57ef08388268bab1780dbd1e6c6f423d5ff47bff
Alvaro Hernandez <alvaro03ha@gmail.com>
2024-06-01 18:24:05 +0200

    boton buscar y add

---
+++ angular/src/app/exercise/add-button/add-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-button.component.html',
+  styleUrl: './add-button.component.css'
+})
+export class AddButtonComponent {
+
+}
--- angular/src/app/exercise/exercise.component.ts
+++ angular/src/app/exercise/exercise.component.ts
@@ -3,17 +3,23 @@ import { Component, OnInit } from '@angular/core';
 import { ExerciseService } from '../services/exercise.service';
 import { NgFor, NgIf } from '@angular/common';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms'
+
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { AddButtonComponent } from './add-button/add-button.component';
 
 @Component({
   selector: 'app-exercise',
   standalone: true,
-  imports: [CommonModule, NgFor, NgIf, NgbCollapseModule],
+  imports: [CommonModule, NgFor, NgIf, NgbCollapseModule, FormsModule, SearchBarComponent, AddButtonComponent],
   templateUrl: './exercise.component.html',
   styleUrls: ['./exercise.component.css'],
 })
 export class ExerciseComponent implements OnInit {
   globalExercises: any = [];
   userExercises: any = [];
+  searchQuery: string = ''; // Variable para almacenar la consulta de búsqueda
+  isSearchBarVisible: boolean = false;
 
   constructor(private exerciseService: ExerciseService) {
   }
@@ -33,5 +39,9 @@ export class ExerciseComponent implements OnInit {
   }
   isUserExercisesDefined(): boolean {
     return typeof this.userExercises === 'object' && this.userExercises !== null;
+  }
+
+  toggleSearchBar() {
+    this.isSearchBarVisible = !this.isSearchBarVisible;
   }
 }
