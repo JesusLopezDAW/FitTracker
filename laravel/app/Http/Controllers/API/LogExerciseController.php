d5f322a6a6bb9bbc08c3025b023b3d8a77c2e2ce
Alvaro Hernandez Andres <a.hernandez@binary-factory.de>
2024-06-09 13:24:17 +0200

    logs y navbar

--- laravel/app/Http/Controllers/API/LogExerciseController.php
+++ laravel/app/Http/Controllers/API/LogExerciseController.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\ExerciseLogRequest;
 use App\Models\Exercise_log;
 use App\Models\ExerciseLog;
+use App\Models\Post;
+use App\Models\Workout;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse as HttpJsonResponse;
 use Illuminate\Support\Facades\Auth;
 
@@ -49,20 +52,54 @@ class LogExerciseController extends Controller
 
         $this->validateExist($log);
         $this->isFromUser($log, Auth::id());
-        
+
         $log->delete();
 
         return JsonResponse::success($log, 'Success', 200);
     }
 
+    public function getByWorkoutId($workoutId)
+    {
+        // Obtener el tiempo de publicación del post
+        $postTime = Post::where('workout_id', $workoutId)->value('created_at');
+
+        // Convertir el tiempo de publicación del post a una instancia de Carbon
+        $postTime = Carbon::parse($postTime);
+
+        // Obtener los registros de ejercicio por el ID de entrenamiento
+        $logs = Exercise_log::where('workout_id', $workoutId)->get();
+
+        // Filtrar las series por el mismo día y hora que el post
+        $exercises = [];
+
+        $logs->each(function ($log) use (&$exercises, $postTime) {
+            $filteredSeries = $log->exercise->series->filter(function ($series) use ($postTime) {
+                return $series->created_at->toDateString() == $postTime->toDateString() &&
+                    $series->created_at->toTimeString() == $postTime->toTimeString();
+            });
+
+            // Agregar las series filtradas al array de ejercicios solo si hay series que coincidan
+            if (!$filteredSeries->isEmpty()) {
+                $exercises[] = [
+                    'exercise' => $log->exercise,
+                    'series' => $filteredSeries
+                ];
+            }
+        });
+
+        // Retornar la respuesta JSON
+        return JsonResponse::success($exercises, 'Success', 200);
+    }
+
     private function validateExist(Exercise_Log | string $log)
     {
-        if(!$log){
+        if (!$log) {
             return JsonResponse::error('ERROR: Log doesnt exist', 404);
         }
     }
 
-    private function isFromUser(Exercise_Log $log, int $userId){
+    private function isFromUser(Exercise_Log $log, int $userId)
+    {
         if ($log->user_id != $userId) {
             return JsonResponse::error('Error: This Log is not from this user', 401);
         }
